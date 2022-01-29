10588736ce46a381cc3f76831f8cb0dfeecb45f9
rosnerdev <83503879+rosnerdev@users.noreply.github.com>
2022-01-29 16:24:36 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate, useParams } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/profile">Profile</a>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>hello world!</h1>} />
+        <Route path="/about" element={<h1>this is the about page</h1>} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Profile() {
+  let navigate = useNavigate();
+  let { username } = useParams();
+
+  return(
+    <>
+      <h1>this is the profile of {username}</h1>
+      <button onClick={() => {navigate("/")}}>navigate home</button>  
+    </>
   );
 }
 
